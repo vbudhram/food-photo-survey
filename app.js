a3e30da397eebf686ecc15ba0927c07fcc3bf3a4
Vijay <vbudhram@gmail.com>
2014-07-29 03:52:45 +0000

    Added controllers and images

--- app.js
+++ app.js
@@ -3,10 +3,53 @@
 var PORT = process.env.PORT || 3000;
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+
 var app = express();
 app.use('/', express.static(__dirname + '/app'));
 
+// parse application/json
+app.use(bodyParser.json());
 
-var server = app.listen(PORT, function(){
+app.get('/results', function(req, res) {
+    fs.readFile('./results/results.json', {encoding:'utf8'},function(err, allResults) {
+        if (err) {
+            console.log(err);
+            res.send(400, err);
+        }
+        res.json(allResults);
+    });
+})
+
+// Save user results
+app.post('/save', function(req, res) {
+    if (!req.body.user) {
+        req.body.user = {
+            name: (new Date()).getTime()
+        }
+    }
+    console.log("Saving user : " + JSON.stringify(req.body.user));
+    console.log("Results : " + JSON.stringify(req.body.user));
+
+    // Append result data
+    var allResults = require('./results/results.json');
+    console.log("Current Results : " + JSON.stringify(allResults));
+    allResults.push(req.body);
+
+    fs.writeFile("./results/results.json", JSON.stringify(allResults), function(err) {
+        if (err) {
+            console.log(err);
+            res.send(400, err);
+        }
+        else {
+            console.log("The file was saved!");
+            res.send(200);
+        }
+    });
+})
+
+var server = app.listen(PORT, function() {
     console.log('Server started on port : ' + PORT);
 });
